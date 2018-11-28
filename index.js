97c51aa07e038051ba3d71d5a9bdf69e0ecf9986
TomaszTR <t.rokita@stremio.com>
2018-11-28 21:25:51 +0100

    fixed dtube

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var manifest = {
     idProperty: ["dtube_id"], // the property to use as an ID for your add-on; your add-on will be preferred for items with that property; can be an array
     // We need this for pre-4.0 Stremio, it's the obsolete equivalent of types/idProperty
     filter: { "query.dtube_id": { "$exists": true }, "query.type": { "$in": ["channel"] } },
-    //endpoint : "http://secret-anchorage-23982.herokuapp.com/stremioget/stremio/v1",
+    endpoint : "http://secret-anchorage-23982.herokuapp.com/stremioget/stremio/v1",
     // Adding a sort would add a tab in Discover and a lane in the Board for this add-on
     sorts: [ {prop: "popularities.dtube", name: "dTube", types: ["channel"]}],
     listedOn: ["web", "desktop", "android", "ios"],
@@ -45,7 +45,7 @@ gateways = [
       "https://chappy.i.ipfs.io"
     ];
    function gatewayByHash(ipfsHash) {
-    var g = ipfsHash.charCodeAt(ipfsHash.length-1) % gateways.length
+    //var g = ipfsHash.charCodeAt(ipfsHash.length-1) % gateways.length
     return 'video.dtube.top'
   }
 
@@ -67,7 +67,6 @@ function getVideoByIdWithCallback(id,callback)
             var videos = (JSON.parse(b)).results;
             if(videos.length != 0)
             {
-                console.log(videos);
                 var video = 
                             {
                                 id: videos[0].meta.video._id,
@@ -103,9 +102,11 @@ function loadPaginatedVideos(users,counter,callback,page,maxAmount,returnArr)
             users.sort(function(a,b){
                 a.popularities - b.popularities;
             })
+            console.log(users);
             callback(null,users);
         }
         else{
+            console.log(users[0]);
             callback(null,users[0]);
         }
     }
@@ -117,13 +118,13 @@ function loadPaginatedVideos(users,counter,callback,page,maxAmount,returnArr)
             var videos = (JSON.parse(b)).results;
             var pages = (JSON.parse(b)).pages;
             var user = users[counter];
+            console.log(videos.length +'a');
                     if(videos.length != 0){
                         for(i = 0, j = videos.length; i < j; i++)
                         {
-
                             var video = 
                             {
-                                id: videos[i].meta.video._id,
+                                id: videos[i].meta.video.content.video480hash ? linkByHash(videos[i].meta.video.content.video480hash) : linkByHash(videos[i].meta.video.content.videohash),
                                 title: videos[i].title,
                                 publishedAt: new Date(videos[i].created),
                                 thumbnail: 'https://snap1.d.tube/ipfs/'+videos[i].meta.video.info.snaphash,
@@ -195,6 +196,7 @@ function createChannelFromAuthorName(author, callback, returnArr,search)
             user.videos = [];
             users.push(user);
         }
+        console.log(users.length);
         if(!returnArr){
                 loadPaginatedVideos(users,0,callback,1,50,returnArr);
         }
@@ -248,9 +250,15 @@ var addon = new Stremio.Server({
     "meta.find": function(args, callback) {
         console.log("received request from meta.find", args)
         if (! args.query) return callback(new Error("no query"));
-        var page = ((args.skip || 0))/10 + 1;
+        if(args.skip)
+        {
+            page = Math.ceil(args.skip / 10) + 1
+        }
+        else
+            page = 1
         console.log("Page ", page);
-        loadPaginatedUsersWith([],page,70,callback);
+        if(page < 10)
+         loadPaginatedUsersWith([],page,60,callback);
     },
     "meta.get": function(args, callback) {
         console.log("received request from meta.get", args, args.query.dtube_id);
@@ -266,7 +274,7 @@ var addon = new Stremio.Server({
 
 if (require.main===module) var server = require("http").createServer(function (req, res) {
     //needed only for the local add-on
-    /*res.setHeader('Access-Control-Allow-Origin', '*');
+  /*  res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Request-Method', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
     res.setHeader('Access-Control-Allow-Headers', '*');*/
